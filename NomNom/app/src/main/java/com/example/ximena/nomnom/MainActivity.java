0d45dbfbd4e650a99be54fec8722d93c96c5f6de
Xbolanos <xbolanosfonseca@gmail.com>
2018-06-09 07:59:36 -0600

    Auto stash before merge of "master" and "origin/master"

--- NomNom/app/src/main/java/com/example/ximena/nomnom/MainActivity.java
+++ NomNom/app/src/main/java/com/example/ximena/nomnom/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.ximena.nomnom;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.ximena.nomnom.interfaces.IAPICaller;
+import com.example.ximena.nomnom.services.HerokuService;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -14,10 +18,16 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IAPICaller {
     CallbackManager callbackManager;
+    private static final int LOGIN_USER_CODE = 200;
+    private static final int REGISTER_USER_CODE = 300;
+    private static final String RELATIVE_API = "auth/signin";
+    private String name, lastName, email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +70,40 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
     public void openHome(View view) {
-        Intent activity = new Intent(this, HomeActivity.class);
-        startActivity(activity);
+        JSONObject user = new JSONObject();
+        TextView txtemail=findViewById(R.id.email);
+        TextView txtpass=findViewById(R.id.pass);
+
+        String new_email=  txtemail.getText().toString();
+        String new_pass=txtpass.getText().toString();
+        email=new_email;
+        password=new_pass;
+        try {
+
+            user.put("email", email);
+            user.put("password", password);
+
+
+            HerokuService.post(RELATIVE_API, user, LOGIN_USER_CODE, this);
+            Intent activity = new Intent(this, HomeActivity.class);
+            startActivity(activity);
+        }catch (Exception e){
+
+        }
     }
     public void openRegister(View view) {
         Intent activity = new Intent(this, RegisterActivity.class);
         startActivity(activity);
+    }
+
+    @Override
+    public void onSuccess(int requestCode, JSONObject response) {
+        Log.d("RESPONSE", String.valueOf(response));
+
+    }
+
+    @Override
+    public void onFailure(int requestCode, Object error) {
+
     }
 }
