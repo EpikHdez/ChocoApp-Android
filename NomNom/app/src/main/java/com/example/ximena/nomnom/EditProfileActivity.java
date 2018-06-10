98feccb66cb2461d3594691c4c1fbe2a61d85005
Xbolanos <xbolanosfonseca@gmail.com>
2018-06-10 00:21:40 -0600

    Jala informacion de perfil

--- NomNom/app/src/main/java/com/example/ximena/nomnom/EditProfileActivity.java
+++ NomNom/app/src/main/java/com/example/ximena/nomnom/EditProfileActivity.java
@@ -5,25 +5,40 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.ximena.nomnom.interfaces.IAPICaller;
+import com.example.ximena.nomnom.services.HerokuService;
 import com.mindorks.placeholderview.PlaceHolderView;
+import com.squareup.picasso.Picasso;
 
-public class EditProfileActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+public class EditProfileActivity extends AppCompatActivity implements IAPICaller {
     private PlaceHolderView mDrawerView;
     private DrawerLayout mDrawer;
     private Toolbar mToolbar;
     private PlaceHolderView mGalleryView;
+    private static final int ADDRESS_USER_CODE = 500;
+    ManagerUser managerUser;
+    private static final String RELATIVE_API = "my/user_address";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
+        managerUser=ManagerUser.getInstance();
         mDrawer = (DrawerLayout)findViewById(R.id.drawerLayout);
         mDrawerView = (PlaceHolderView)findViewById(R.id.drawerView);
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
         setupDrawer();
+        HerokuService.get(RELATIVE_API, ADDRESS_USER_CODE, this);
 
     }
     private void setupDrawer(){
@@ -52,4 +67,37 @@ public class EditProfileActivity extends AppCompatActivity {
         mDrawer.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
     }
+
+    @Override
+    public void onSuccess(int requestCode, JSONObject response) {
+        try {
+            switch (requestCode) {
+                case ADDRESS_USER_CODE:
+
+                    Log.d("response", response.toString());
+                    JSONArray addresses = response.getJSONArray("addresses");
+                    TextView txtname = findViewById(R.id.name);
+                    TextView txtemail = findViewById(R.id.email);
+                    TextView txthome = findViewById(R.id.home);
+                    TextView txtjob = findViewById(R.id.job);
+                    ImageView imageView = findViewById(R.id.image);
+
+                    txtname.setText(managerUser.getName() + " " + managerUser.getLastname());
+                    txtemail.setText(managerUser.getEmail());
+                    Picasso.with(this).load(managerUser.getPicture()).into(imageView);
+                    if(addresses.length()==0) {
+                        txthome.setText("No ha insertado su dirección.");
+                        txtjob.setText("No ha insertado su dirección.");
+                    }
+                    break;
+            }
+        }catch (Exception e){}
+
+    }
+
+    @Override
+    public void onFailure(int requestCode, Object error) {
+
+    }
+    
 }
